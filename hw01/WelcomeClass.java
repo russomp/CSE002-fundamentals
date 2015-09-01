9be59e2725f06a918f9a6f45a01f10c02237eda3
mpr315 <mpr315@lehigh.edu>
2015-09-01 23:41:09 +0000

    final WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Michael Russo
+//Homework 01
+//Welcome Class Java Program
+//
+//Instructions:
+//  compile the program:
+//    javac WelcomeClass.java
+//  run program:
+//    java WelcomeClass
+//
+//
+//define class
+public class WelcomeClass{
+    //add main method
+    public static void main(String[] args){
+        //print welcome messgae
+        System.out.println("   -----------");
+        System.out.println("   | WELCOME |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-M--P--R--3--1--5-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("This is my first computer programming class.");
+    }
+}
