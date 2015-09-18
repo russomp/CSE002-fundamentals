46013977a99e1adcdf9f045364402da14ee0d63e
mpr315 <mpr315@lehigh.edu>
2015-09-18 17:17:03 +0000

    final lab04-Calculator

---
+++ lab04-Calculator/Calculator.java
@@ -0,0 +1,59 @@
+//Michael Russo
+//Lab 04
+//
+//Calculator Java Program:
+//  takes user inputs: 
+//    (1) integer value, a
+//    (2) integer value, b
+//    (3) operator [+, -, *, /]
+//  computes value of a+b, a-b, a*b, OR a/b based on operator
+//
+//Instructions:
+//  Compile Program:
+//    javac Calculator.java
+//  Run Program:
+//    java Calculator
+//  Input integer value one
+//  Input integer value two
+//  Input operator value
+
+//import scanner class to read user inputs
+import java.util.Scanner;
+
+//define Timer class
+public class Calculator{
+    
+    //define main method of Timer class
+    public static void main(String[] args){
+        
+        //declare scanner
+        Scanner myScanner = new Scanner(System.in);
+        
+        //prompt user for inputs and store input values
+        System.out.print("Enter integer, a: ");
+        int myIntA = myScanner.nextInt();
+        System.out.print("Enter integer, b: ");
+        int myIntB = myScanner.nextInt();
+        System.out.print("Enter operator [+, -, *, OR /]: ");
+        String myOperator = myScanner.next();
+        int total = 0;
+        
+        //create a switch statement to operate on a and b based on operator given
+        switch(myOperator){
+            case "+": total = myIntA + myIntB;
+            System.out.println("a + b = " + total);
+            break;
+            case "-": total = myIntA - myIntB;
+            System.out.println("a - b = " + total);
+            break;
+            case "*": total = myIntA * myIntB;
+            System.out.println("a * b = " + total);
+            break;
+            case "/": total = myIntB / myIntA;
+            System.out.println("a / b = " + total);
+            break;
+            default: 
+            System.out.println("Invalid entry.");
+        }
+    }
+}
