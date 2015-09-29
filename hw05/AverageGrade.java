3ed11aae412474ce622ed776d45b0a3e2ece57ac
mpr315 <mpr315@lehigh.edu>
2015-09-29 08:05:19 +0000

    final hw05

--- hw05/AverageGrade.java
+++ hw05/AverageGrade.java
@@ -18,6 +18,9 @@
 //import scanner class to read user inputs
 import java.util.Scanner;
 
+//import input mismatch error for scanner class
+import java.util.InputMismatchException;
+
 //define AverageGrade class
 public class AverageGrade{
     
@@ -28,8 +31,7 @@ public class AverageGrade{
         Scanner myScanner = new Scanner(System.in);
         
         //declare and initialize all variables in scope
-        String gradeInput = "0";
-        double grade = 0;
+        double gradeInput = 0;
         double gradesTotal = 0;
         double gradesAverage = 0;
         double numInputs = 0;
@@ -37,53 +39,38 @@ public class AverageGrade{
         //display termination condition
         System.out.println("***Enter '999' when done entering grades***");
         
-        //prompt user for grade input until termination condition is met
-        while (!gradeInput.equals("999")){
-            //declare and initialize loop condition for user input test
-            boolean badGradeInput = true;
-            
-            //prompt user for valid input
-            do{
-                //prompt user for grade and store value as type 'String' 
+        //prompt user for valid input
+        do{
+            //check if user input is a valid number
+            try{
+                //prompt user for grade and store value as type 'Double' 
                 System.out.print("Enter student grade [0-100]: ");
-                gradeInput = myScanner.next();
+                gradeInput = myScanner.nextDouble();
                 
-                //check if user input is a valid number
-                try{
-                    //convert grade to double
-                    grade = Double.parseDouble(gradeInput);
-                    
-                    //check for termination condition
-                    if (grade == 999){
-                        //exit loop if user inputs termination condition
-                        break;
-                    }
-                    //check if user input is in range 0 to 100
-                    else if (grade < 0 || grade > 100){
+                if (gradeInput != 999){
+                    //check for grades out of possible range
+                    if ((gradeInput < 0 || gradeInput > 100)){
                         //print error statement if grade is out of range
                         System.out.println("Error: Grade out of range.");
                         //return to top of loop and prompt user for new input
                         continue;
                     }
-                    
-                    //change loop condition if input is valid
-                    badGradeInput = false;
+                    else{
+                        //sum valid grade values entered by user
+                        gradesTotal += gradeInput;
+                        //count number of valid user inputs
+                        numInputs ++;
+                    }
                 }
-                catch (NumberFormatException e){
-                    //print error statement
-                    System.out.println("Error: Enter a valid grade value to continue or enter '999' to terminate.");
-                }
-            } while(badGradeInput);
-            
-            //compute new total and number of inputs unless termination condition is entered 
-            if (grade != 999){
-                //sum valid grade values entered by user
-                gradesTotal += grade;
-                //count number of valid user inputs
-                numInputs ++;
             }
-        }
-        
+            //check for invalid entries of type other than 'Double'
+            catch (InputMismatchException e){
+                //print error statement
+                System.out.println("Error: Enter a valid grade value to continue or enter '999' to terminate.");
+                myScanner.next();
+            }
+        } while (gradeInput != 999);
+    
         //check if any grades were inpute
         if (numInputs == 0){
             //print error message if no grades are entered
